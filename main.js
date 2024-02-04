aa739cfabddf76cf6b8c9d78a82c5a7f7a4073eb
144234 <78003838+144234@users.noreply.github.com>
2024-02-04 16:40:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+results=[];
 status = "";
 
 function setup(){
@@ -8,6 +9,14 @@ function setup(){
     video.hide();
 }
 
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    results = results;
+}
+ 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -21,5 +30,21 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
-
+    if(results[i].label == status)
+    {
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < results.length; i++) {
+            document.getElementById("status").innerHTML = status + " Found"; 
+ 
+            fill("#FF0000");
+            percent = floor(results[i].confidence * 100);
+            text(results[i].label + " " + percent + "%" , results[i].x + 15, results[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(results[i].x, results[i].y, results[i].width, results[i].height);
+        }
+    } 
+    else(){
+        document.getElementById("status").innerHTML = status + " Not Found"; 
+    }
 }
